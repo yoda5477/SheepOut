7f8d3eb619584d4993c76f800c08c2ad0ff91c34
yoda5477 <sylvain5477@gmail.com>
2015-04-06 11:24:55 +0200

    Hop rotation du mouton ajoutée

--- Assets/SheepBehavior.cs
+++ Assets/SheepBehavior.cs
@@ -10,12 +10,14 @@ public class SheepBehavior : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDr
 	float angle;
 	Vector2 target = new Vector2 (0, 0);
 	public int Force = 3;
+	public float dist = 0;
+	public float mult = 1;
 	
 	public void OnBeginDrag (PointerEventData eventData){
 		//		Debug.Log ("MouseOnUnit.OnBeginDrag");
 		if (moveLine == null) {
 			moveLine = (GameObject) Instantiate (dragLineProto, transform.position, transform.rotation);
-			moveLine.transform.SetParent(transform);
+			moveLine.transform.SetParent(transform.parent);
 		}
 		//		Time.timeScale = 0;
 		moveLine.transform.position = transform.position;
@@ -33,8 +35,6 @@ public class SheepBehavior : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDr
 		target.x = eventData.position.x;
 		target.y = eventData.position.y;
 		//Time.timeScale = 1;
-		
-		//		this.GetComponent<Rigidbody2D> ().Sleep ();
 	}
 	
 	public void Update() {
@@ -42,13 +42,21 @@ public class SheepBehavior : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDr
 			angle = Vector2.Angle (new Vector2 (0, 1), new Vector2 (target.x - transform.position.x, target.y - transform.position.y));
 			cross = Mathf.Sign (Vector3.Cross (new Vector2 (0, 1), new Vector2 (target.x - transform.position.x, target.y - transform.position.y)).z);
 			moveLine.GetComponent<RectTransform> ().sizeDelta = new Vector2 (moveLine.GetComponent<RectTransform> ().rect.width, Vector2.Distance (new Vector2 (target.x, target.y), new Vector2 (transform.position.x, transform.position.y)));
-			moveLine.GetComponent<RectTransform> ().localEulerAngles = new Vector3 (0, 0, angle * cross);
+			moveLine.GetComponent<RectTransform> ().localEulerAngles = new Vector3 (0, 0, mult*transform.rotation.z+angle * cross);
 		}
 	}
 	
 	public void FixedUpdate() {
 		if (moveLine != null) {
-			this.GetComponent<Rigidbody2D> ().AddForce (new Vector2 ((target.x - transform.position.x)*Force, (target.y - transform.position.y)*Force),ForceMode2D.Impulse);
+			moveLine.transform.position = transform.position;
+			dist = Vector2.Distance (new Vector2 (target.x, target.y), new Vector2 (transform.position.x, transform.position.y));
+			if ( dist < 10) {
+//				moveLine=null;
+			}
+			else {
+				this.GetComponent<Rigidbody2D> ().AddForce (new Vector2 ((target.x - transform.position.x)*Force, (target.y - transform.position.y)*Force),ForceMode2D.Impulse);
+				//moveLine.GetComponent<RectTransform> ().localEulerAngles = new Vector3 (0, 0, angle * cross);
+			}
 		}
 	}
 }
