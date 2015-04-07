1d5fe25f4392bd22b4ed0ea187b5548dbb4eaadb
yoda5477 <sylvain5477@gmail.com>
2015-04-07 18:11:23 +0200

    Ordre perso compteur
    Déplacement direction et compteur sous le corps de mouton

---
+++ Assets/ChronoBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChronoBehavior : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		GetComponent<RectTransform> ().localEulerAngles = new Vector3 (0, 0, -transform.parent.rotation.eulerAngles.z );
+
+	}
+}
